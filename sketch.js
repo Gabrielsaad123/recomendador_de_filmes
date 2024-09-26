a5f3233fb58cfdbf70e135ffe1c38ad2e8245f77
Gabrielsaad123 <161738145+Gabrielsaad123@users.noreply.github.com>
2024-09-26 09:24:03 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+// ação, aventura, fantasia, comédia de ação, animação, drama, ficção científica
+
+// vingadores ultimato, 12, ação e aventura
+// vingadores guerra infinita, 12, ação e fantasia 
+
+// kung fu panda, LIVRE, comédia de ação, aventura e animação
+// madacascar, LIVRE,comédia, aventura e animação
+// velozes e furiosos, 14, ação, aventura e drama
+// maze runner, 16, ação e ficção científica
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
